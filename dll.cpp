cba3de1339755b73c06024c760cd547b3f3a775b
Know me? <he72nkhe5biw7@gmail.com>
2026-03-25 16:23:27 +0100

    Update dll.cpp

--- dll.cpp
+++ dll.cpp
@@ -1,1 +1,18 @@
+#include <windows.h>
 
+DWORD WINAPI MainThread(LPVOID param) {
+    MessageBoxA(0, "Injected", "ShadowInject", MB_OK);
+
+    while (true) {
+        Sleep(1000);
+    }
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID reserved) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        CreateThread(0, 0, MainThread, hModule, 0, 0);
+    }
+    return TRUE;
+}
